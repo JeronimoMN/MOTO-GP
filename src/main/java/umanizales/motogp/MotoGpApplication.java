18a27dbab0ebca425311b1b93963dcf23b3f5119
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-09-15 21:59:09 -0500

    first commit

---
+++ src/main/java/umanizales/motogp/MotoGpApplication.java
@@ -0,0 +1,13 @@
+package umanizales.motogp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoGpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoGpApplication.class, args);
+    }
+
+}
